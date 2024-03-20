8561acbf48d66ac3fe7e6221634d31b64f4b7627
cmhehe176 <example@email.com>
2024-03-20 09:24:08 +0700

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from "@nestjs/common";
+import { StudentModule } from './student/student.module';
+import { ClassModule } from './class/class.module';
+import { TeacherModule } from './teacher/teacher.module';
+import { SubjectModule } from './subject/subject.module';
 
 @Module({
 	
+
+  imports: [StudentModule, ClassModule, TeacherModule, SubjectModule]
 })
 
 export class AppModule{}
